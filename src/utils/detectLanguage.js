64cb472cd91451a6d8417122a10f28c63d5947d5
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-03-08 23:28:11 +0100

    forgot to link parsers

--- src/utils/detectLanguage.js
+++ src/utils/detectLanguage.js
@@ -6,10 +6,15 @@ const parsers = loadAllParsers();
 * @returns
 */
 function detectLanguageByPath(filePath) {
-  if (filePath.endsWith(".js")) return parsers.get("javascript");
+  if (filePath.endsWith(".js")) return parsers.get("javascript");;
+  if (filePath.endsWith(".cs")) return parsers.get("c-sharp");
+  if (filePath.endsWith(".py")) return parsers.get("python");
+  if (filePath.endsWith(".c")) return parsers.get("c");
+  if (filePath.endsWith(".java")) return parsers.get("java");
+  if (filePath.endsWith(".cpp")) return parsers.get("cpp");
+  if (filePath.endsWith(".rb")) return parsers.get("ruby");
 }
 
 module.exports = {
-  detectLanguageByPath: detectLanguageByPath,
-  getLanguageParser: () => { }
+  detectLanguageByPath: detectLanguageByPath
 }
