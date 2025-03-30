5703ff7bf50ab676664cce18a65e37fa4e875572
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-03-30 05:23:46 +0200

    refactor: extract script from html
    - added type to `acquireVsCodeApi`

---
+++ commands/panels/index.js
@@ -0,0 +1,15 @@
+const vscode = acquireVsCodeApi();
+const counter = document.getElementById("lines-of-code-counter");
+
+let count = 0;
+let intervalID = setInterval(() => {
+  counter.textContent = (count++).toString();
+
+  // stop the interval;
+  if (count > 100) return clearInterval(intervalID);
+
+  // Alert the extension when our cat introduces a bug
+  if (Math.random() < 0.001 * count) {
+    vscode.postMessage(`🐛  Bug on line:  ${count}`);
+  }
+}, 250);
