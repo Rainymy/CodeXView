b9b89b8beff0d1243ea62f10c7cea2c9cd31a52a
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-04-08 04:34:30 +0200

    Update codeParser.js

--- src/utils/codeParser.js
+++ src/utils/codeParser.js
@@ -1,3 +1,5 @@
+const { Tree } = require("web-tree-sitter");
+
 const { readFileSync } = require("./fileHandler");
 const { detectLanguageByPath } = require("./detectLanguage");
 
@@ -28,6 +30,10 @@ function analyzeCode(filePath) {
   return parser.parse(readFileSync(path, "utf8"));
 }
 
+/**
+* @param {Tree} tree
+* @returns
+*/
 function syntaxTreeToJson(tree) {
   if (!tree || !tree.rootNode) {
     throw new Error("Invalid syntax tree");
@@ -35,7 +41,12 @@ function syntaxTreeToJson(tree) {
   return syntaxNodeToJson(tree.rootNode);
 }
 
-// ✅ Helper function to recursively convert a SyntaxNode to JSON
+/**
+* ✅ Helper function to recursively convert a SyntaxNode to JSON
+* @typedef {import("web-tree-sitter").Node} Node
+* @param {Node} node
+* @returns
+*/
 function syntaxNodeToJson(node) {
   // TODO: CONVERT THIS INTO WHILE LOOP
   // (Recursively is bad idea with huge data)
@@ -46,7 +57,38 @@ function syntaxNodeToJson(node) {
   };
 }
 
+/**
+* @param {Tree} tree
+* @returns
+*/
+function syntaxTreeToJson_NEW(tree) {
+  if (!tree || !tree.rootNode) {
+    throw new Error("Invalid syntax tree");
+  }
+
+  /**
+  * ✅ Helper function to recursively convert a SyntaxNode to JSON
+  * @param {Node} node
+  * @returns
+  */
+  function syntaxNodeToJson_NEW(node) {
+    // TODO: CONVERT THIS INTO WHILE LOOP
+    // (Recursively is bad idea with huge data)
+    // if (node.childForFieldName("name")?.text) {
+    //   console.log(node.childForFieldName("name")?.text)
+    // }
+    return {
+      type: node.type,
+      name: node.childForFieldName("name")?.text, // Extract raw text
+      children: node.children.map(syntaxNodeToJson_NEW) // Recursively process children
+    };
+  }
+
+  return syntaxNodeToJson_NEW(tree.rootNode);
+}
+
 module.exports = {
   analyzeCode: analyzeCode,
   syntaxTreeToJson: syntaxTreeToJson
+  // syntaxTreeToJson: syntaxTreeToJson_NEW // <-- try this
 };
