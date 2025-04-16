c595e27413f7a688f5249245422a9f8fc065e35a
Rainymy <48211766+Rainymy@users.noreply.github.com>
2025-04-16 15:37:54 +0200

    refactor: working on it

--- commands/codebase.js
+++ commands/codebase.js
@@ -1,13 +1,21 @@
-const { compareDiagramObjects, extractClassInfoFromPlantUML, extractJSONArrayInfo } = require("../src/components/DiagramChecker");
+const {
+  compareDiagramObjects,
+  extractClassInfoFromPlantUML,
+  extractJSONArrayInfo,
+  DiagramObjects,
+} = require("../src/components/DiagramChecker");
 
-const { generateCCDiagram, isValidPlantUMLCode } = require("../src/components/diagramGenerator");
+const {
+  generateCCDiagram,
+  isValidPlantUMLCode,
+} = require("../src/components/diagramGenerator");
 const AIConnection = require("../src/components/AIConnection");
 
 const { readPrompt } = require("../src/utils/fileHandler");
 const { analyzeCodebase } = require("../src/utils/codebaseParser");
 const { loadIgnoreRules } = require("../src/utils/ignoreRules");
 
-const { getWorkspaceFolder } = require('../src/fallbacks/activeDocument');
+const { getWorkspaceFolder } = require("../src/fallbacks/activeDocument");
 
 const { Notify, parseSetup } = require("./vsUtil");
 
@@ -38,48 +46,20 @@ async function codebaseAnalysis() {
 
   AIConnection.setPrompt(readPrompt());
 
-  const allSyntaxTreeJSON = parsedCode.map(v => v.json);
+  const allSyntaxTreeJSON = parsedCode.map((v) => v.json);
   const diagramObj = await extractJSONArrayInfo(allSyntaxTreeJSON);
   console.log("Diagram Object:", diagramObj);
-  let validUMLCode = "";
-  let isNotCorrect = true;
-  let attempts = 0;
-  let valid = false;
-  const maxAttempts = 5;
 
-  while (isNotCorrect && attempts < maxAttempts) {
-    attempts++;
+  const diagramCode = await compareAndRetryIfFail(diagramObj, allSyntaxTreeJSON)
 
-    const diagram = await AIConnection.getChatResponse(allSyntaxTreeJSON);
-    console.log("workspace diagram:", diagram);
-
-    const umlObj = await extractClassInfoFromPlantUML(diagram);
-    
-    console.log("umlObj:", umlObj);
-    valid = await isValidPlantUMLCode(diagram);
-    const matches = await compareDiagramObjects(diagramObj, umlObj);
-    
-    if (valid && matches) {
-      isNotCorrect = false;
-      validUMLCode = diagram;
-    } else {
-      console.log(`Attempt ${attempts}: Diagram did not pass validation.`);
-      console.log("Valid:", valid);
-      console.log("Matches:", matches);
-      console.log("Diagram:", umlObj.classCounter);
-      console.log("Parsed:", diagramObj.classCounter);
-    }
-  }
-
-  if (!valid) {
+  if (diagramCode === null) {
     Notify.info("CodeXView! Failed to generate Diagram.");
     return;
   }
 
-
   // add diagram to project
-  console.log("DiagramCode:", validUMLCode);
-  const added = await generateCCDiagram(validUMLCode);
+  console.log("DiagramCode:", diagramCode);
+  const added = await generateCCDiagram(diagramCode);
 
   if (!added) {
     Notify.error("CodeXView! Error adding diagram to project...");
@@ -87,6 +67,41 @@ async function codebaseAnalysis() {
   }
 
   Notify.info("CodeXView! Finished, Diagram has been added to your project...");
+}
+
+/**
+ * @typedef {import("../parsers/utils").SyntaxTreeJSON} SyntaxTreeJSON
+ * @param {DiagramObjects} diagramObj
+ * @param {SyntaxTreeJSON|SyntaxTreeJSON[]} allSyntaxTreeJSON
+ */
+async function compareAndRetryIfFail(diagramObj, allSyntaxTreeJSON) {
+  let attempts = 1;
+  const maxAttempts = 5;
+
+  while (attempts <= maxAttempts) {
+    attempts++;
+
+    const diagram = await AIConnection.getChatResponse(allSyntaxTreeJSON);
+    console.log("workspace diagram:", diagram);
+
+    const umlObj = await extractClassInfoFromPlantUML(diagram);
+
+    console.log("umlObj:", umlObj);
+    const valid = await isValidPlantUMLCode(diagram);
+    const matches = await compareDiagramObjects(diagramObj, umlObj);
+
+    if (!valid || !matches) {
+      console.log(`Attempt ${attempts}: Diagram did not pass validation.`);
+      console.log("Valid:", valid);
+      console.log("Matches:", matches);
+      console.log("Diagram:", umlObj.classCounter);
+      console.log("Parsed:", diagramObj.classCounter);
+    }
+
+    return diagram;
+  }
+
+  return null;
 }
 
 module.exports = codebaseAnalysis;
